05350cbe1e8152ad55932d7bb7316482de276d8b
Ania <ania.jurago@gmail.com>
2023-12-09 18:18:41 +0900

    :fire: Removed typescript

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import van from 'vanjs-core'
-
-const { a, div, li, p, ul } = van.tags
-
-const Hello = () => div(p('👋Hello'), ul(li('🗺️World'), li(a({ href: 'https://vanjs.org/' }, '🍦VanJS'))))
-
-van.add(document.body, Hello())
