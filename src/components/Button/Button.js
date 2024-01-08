44550da7e47c08d5872a012ba313659de206649e
Ania <ania.jurago@gmail.com>
2024-01-08 16:39:44 +0900

    :lipstick: Added additional params to a Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,5 +4,5 @@ import './button.css'
 
 const { button } = van.tags
 
-export const Button = ({ text, onClick, type }) =>
-  button({ onclick: onClick, class: `button ${type || 'default'}` }, text)
+export const Button = ({ text, onClick, type, name = '', disabled = false, alt = '' }) =>
+  button({ onclick: onClick, class: `button ${type || 'default'}`, disabled, name, alt }, text)
--- src/main.js
+++ src/main.js
@@ -8,11 +8,50 @@ const Hello = () =>
   div(
     { class: 'root' },
     div(NumberInput({ min: '1', name: 'example', alt: 'number input example' })),
-    div(Button({ text: 'create room', onClick: () => alert('Hello from 🍦VanJS') })),
-    div(Button({ text: 'create room', type: 'success', onClick: () => alert('Hello from 🍦VanJS') })),
-    div(Button({ text: 'create room', type: 'warning', onClick: () => alert('Hello from 🍦VanJS') })),
-    div(Button({ text: 'create room', type: 'error', onClick: () => alert('Hello from 🍦VanJS') })),
-    div(Button({ text: 'create room', type: 'info', onClick: () => alert('Hello from 🍦VanJS') }))
+    div(
+      Button({
+        text: 'create room',
+        name: 'example button',
+        alt: 'default button example',
+        onClick: () => alert('Hello from 🍦VanJS')
+      })
+    ),
+    div(
+      Button({
+        text: 'create room',
+        name: 'example button',
+        alt: 'success button example',
+        type: 'success',
+        onClick: () => alert('Hello from 🍦VanJS')
+      })
+    ),
+    div(
+      Button({
+        text: 'create room',
+        name: 'example button',
+        alt: 'warning button example',
+        type: 'warning',
+        onClick: () => alert('Hello from 🍦VanJS')
+      })
+    ),
+    div(
+      Button({
+        text: 'create room',
+        name: 'example button',
+        alt: 'error button example',
+        type: 'error',
+        onClick: () => alert('Hello from 🍦VanJS')
+      })
+    ),
+    div(
+      Button({
+        text: 'create room',
+        name: 'example button',
+        alt: 'info button example',
+        type: 'info',
+        onClick: () => alert('Hello from 🍦VanJS')
+      })
+    )
   )
 
 van.add(document.body, Hello())
